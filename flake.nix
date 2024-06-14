ba4ee72ab8748a987c632a3194028f547529e93b
Tim Koval <timkoval00@gmail.com>
2024-06-15 03:28:09 +0500

    chore: new release

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix-flake-based Rust development environment";
 
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -34,6 +34,7 @@
             cargo-watch
             cargo-make
             rust-analyzer
+            trunk
           ];
         };
       });
