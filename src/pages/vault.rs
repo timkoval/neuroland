b5a92483d784025654a6b918073a28aae6da9f75
Tim Koval <timkoval00@gmail.com>
2024-07-17 00:37:19 +0500

    feat: add links to vault page

--- src/pages/vault.rs
+++ src/pages/vault.rs
@@ -15,47 +15,43 @@ pub fn Vault() -> impl IntoView {
             <div
                 class="flex flex-grow mb-auto justify-start items-center"
             >
-                <div class="flex flex-col justify-start transition-colors duration-200">
-                    <div class="flex flex-col md:flex-row justify-start md:space-x-6">
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                             <a
-                                class="flex flex-row items-center text-gruvbox-fg3 md:hover:text-gruvbox-green active:text-gruvbox-green transition-colors duration-200 mb-6"
-                                href="https://neurogarden.timkoval.rs/"
+                                class="items-center text-gruvbox-fg3 md:hover:text-gruvbox-yellow2 active:text-gruvbox-yellow2 transition-colors duration-200"
+                                href="https://timkoval.rs/"
                             >
                                 <div class="flex flex-col items-start">
-                                    <p class="tracking-wide md:text-2xl text-xl">"neurogarden"</p>
-                                    <p class="text-start md:w-full w-48">"garden for thoughts and notes"</p>
+                                    <p class="tracking-wide md:text-2xl text-xl">"this website"</p>
+                                    <p class="text-start md:w-72 w-48">"personal page made with Leptos, Trunk and Tailwind"</p>
                                 </div>
                             </a>
                             <a
-                                class="flex flex-row items-center text-gruvbox-fg3 md:hover:text-gruvbox-green active:text-gruvbox-green transition-colors duration-200 mb-6"
-                                href="https://neurogarden.timkoval.rs/"
+                                class="items-center text-gruvbox-fg3 md:hover:text-gruvbox-yellow2 active:text-gruvbox-yellow2 transition-colors duration-200"
+                                href="https://github.com/timkoval/neuronix"
                             >
                                 <div class="flex flex-col items-start space-y-1">
-                                    <p class="tracking-wide md:text-2xl text-xl">"neurogarden"</p>
-                                    <p class="text-start md:w-full w-48">"garden for thoughts and notes"</p>
-                                </div>
-                            </a>
-                    </div>
-                    <div class="flex flex-col md:flex-row justify-start md:space-x-6">
-                            <a
-                                class="flex flex-row items-center text-gruvbox-fg3 md:hover:text-gruvbox-green active:text-gruvbox-green transition-colors duration-200 mb-6"
-                                href="https://neurogarden.timkoval.rs/"
-                            >
-                                <div class="flex flex-col items-start">
-                                    <p class="tracking-wide md:text-2xl text-xl">"neurogarden"</p>
-                                    <p class="text-start md:w-full w-48">"garden for thoughts and notes"</p>
+                                    <p class="tracking-wide md:text-2xl text-xl">"neuronix"</p>
+                                    <p class="text-start md:w-72 w-48">"Nix(OS|Darwin) config"</p>
                                 </div>
                             </a>
                             <a
-                                class="flex flex-row items-center text-gruvbox-fg3 md:hover:text-gruvbox-green active:text-gruvbox-green transition-colors duration-200 mb-6"
-                                href="https://neurogarden.timkoval.rs/"
+                                class="items-center text-gruvbox-fg3 md:hover:text-gruvbox-yellow2 active:text-gruvbox-yellow2 transition-colors duration-200"
+                                href="https://github.com/timkoval/neuronvim"
                             >
                                 <div class="flex flex-col items-start space-y-1">
-                                    <p class="tracking-wide md:text-2xl text-xl">"neurogarden"</p>
-                                    <p class="text-start md:w-full w-48">"garden for thoughts and notes"</p>
+                                    <p class="tracking-wide md:text-2xl text-xl">"neuronvim"</p>
+                                    <p class="text-start md:w-72 w-48">"Neovim lightweight config"</p>
                                 </div>
                             </a>
-                    </div>
+                            <a
+                                class="items-center text-gruvbox-fg3 md:hover:text-gruvbox-yellow2 active:text-gruvbox-yellow2 transition-colors duration-200"
+                                href="https://github.com/timkoval/aoc-2023"
+                            >
+                                <div class="flex flex-col items-start space-y-1">
+                                    <p class="tracking-wide md:text-2xl text-xl">"aoc-2023"</p>
+                                    <p class="text-start md:w-72 w-48">"Advent of Code 2023 in Rust"</p>
+                                </div>
+                            </a>
                 </div>
             </div>
         </div>
